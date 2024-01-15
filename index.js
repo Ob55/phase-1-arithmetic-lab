3d8a44df9b70373440e64be4caa0067996721208
Ob55 <brian55mwangi@gmail.com>
2024-01-15 11:20:40 +0300

    done assigment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random); 
+
+let num3 = 40;
+let num4 = 6;
+
+let mod = num3 % num4;
+console.log(mod); 
+
+let max = Math.max(1, 2, 3, 4, 10, 20);
+console.log(max); 
